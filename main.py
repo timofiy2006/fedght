481c9bb2ccf22dd41a2e708e50c9590a83315c1e
timofiy2006 <146582193+timofiy2006@users.noreply.github.com>
2024-03-13 22:44:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import module1
+import time
+local_time = time.localtime()
+print (local_time)
+print (module1.shawuha)
+c = 7
+d = 3
+module1.show_user(c,d)
---
+++ module1.py
@@ -0,0 +1,17 @@
+shawuha = ['meat', 'lavash','souse']
+pizza = ['cheese', 'souse', 'bread']
+borsh = ['burak', 'meat', 'potato' ]
+shawufa_opis="самая крутая шавуха в мире"
+pizza_opis="самая крутая пица в мире"
+borsh_opis="самый крутой борщ в мире"
+menu = {'meat': 3,
+        'cheese': 69,
+        'souse': 6,
+        'potato': 5,
+        'burak': 14
+         }
+a=5
+b=10
+def show_user(a,b):
+   c=a+b
+   return  print (c)
